45e3dd84e64db7c2ea40076337c693b87d022819
Unknown <juan.sebastian.ms@gmail.com>
2016-09-18 09:15:10 -0500

    Base

---
+++ Logic/src/main/java/edu/eci/cosw/boxapp/BoxAppApplication.java
@@ -0,0 +1,12 @@
+package edu.eci.cosw.boxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxAppApplication.class, args);
+	}
+}
---
+++ Logic/src/test/java/edu/eci/cosw/boxapp/BoxAppApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.eci.cosw.boxapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoxAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
